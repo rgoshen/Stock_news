25d92552a035f0707ff8a5c3ecd63e14115276d8
Richard Goshen <rick.goshen@gmail.com>
2020-12-08 09:20:36 -0700

    added previous day stock closing

--- main.py
+++ main.py
@@ -19,18 +19,20 @@ stock_params = {
 
 ## STEP 1: Use https://www.alphavantage.co/documentation/#daily
 # When stock price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
-
-#TODO 1. - Get yesterday's closing stock price. Hint: You can perform list comprehensions on Python dictionaries. e.g. [new_value for (key, value) in dictionary.items()]
 stock_resp = requests.get(url=STOCK_ENDPOINT, params=stock_params)
 stock_resp.raise_for_status()
 stock_data = stock_resp.json()["Time Series (Daily)"]
 stock_data_list = [value for (key, value) in stock_data.items()]
+
+#TODO 1. - Get yesterday's closing stock price. 
 yesterday_data = stock_data_list[0]
 yesterday_closing_price = yesterday_data["4. close"]
 print(yesterday_closing_price)
 
 #TODO 2. - Get the day before yesterday's closing stock price
-
+previous_day_data = stock_data_list[1]
+previous_closing_price = previous_day_data["4. close"]
+print(previous_closing_price)
 
 #TODO 3. - Find the positive difference between 1 and 2. e.g. 40 - 20 = -20, but the positive difference is 20. Hint: https://www.w3schools.com/python/ref_func_abs.asp
 
